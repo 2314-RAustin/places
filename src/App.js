906bbf982f30998469a89b42c5cf8c1717412398
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2020-02-11 23:16:34 -0800

    Uso de clases css, evento onclick, y uso de hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Title from './Components/Title.js';
 import logoPlaces from './images/logo-places.png'
 
-function App() {
-  return (
-    <section>
-      <div>
-        <div>
-          <Title></Title>
-          <button>Crear una cuenta gratuita</button>
-          <img src={logoPlaces} alt="logo-places" height="300"/>
-          <div>
-            <ul>
-              <li>
-                <h3>Califica con emociones</h3>
-                <p>Califica tus lugares con experiencias, no con números</p>
-              </li>
-              <li>
-                <h3>Sin internet ? sin problemas</h3>
-                <p>Places funciona sin internet o en conexiones lentas</p>
-              </li>
-              <li>
-                <h3>Tus lugares favoritos</h3>
-                <p>Define tu lista de sitios favoritos</p>
-              </li>
-            </ul>
+  export default () => {
+    const [numero, setNumero] = useState(0);
+    return (
+      <section>
+        <div className="Header-background">
+          <div style={{"width":"80%", "margin":"0 auto"}}>
+            <div className="Header-main">
+              <Title></Title>
+              <h2>{numero}</h2>
+              <button onClick={()=>setNumero(numero + 1)}>Crear una cuenta gratuita</button>
+              <img className="Header-illustration" src={logoPlaces} alt="logo-places" height="300"/>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <h3>Califica con emociones</h3>
+                  <p>Califica tus lugares con experiencias, no con números</p>
+                </li>
+                <li>
+                  <h3>Sin internet ? sin problemas</h3>
+                  <p>Places funciona sin internet o en conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                  <p>Define tu lista de sitios favoritos</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
 }
-
-export default App;
